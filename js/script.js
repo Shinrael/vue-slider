c817e42ee9528a660b345f00cc53cd7db92ae778
Shinrael <mircoelena89@gmail.com>
2024-03-12 18:45:37 +0100

    [add] comment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const { createApp } =  Vue;
+const { createApp } = Vue;
 createApp({
-  data(){
-    return{
+  data() {
+    return {
+      // Array di percorsi delle immagini
       images: [
         'img/spiderman.webp',
         'img/stray.webp',
@@ -9,28 +10,38 @@ createApp({
         'img/fortnite.webp',
         'img/avengers.webp',
       ],
+      // Contatore per tener traccia dell'immagine corrente
       counter: 0,
-      autoPlayEnabled : true,
-    }
+      // Flag per abilitare/disabilitare l'autoplay
+      autoPlayEnabled: true,
+    };
   },
-  methods:{
-    nextImg(isNext){
+  methods: {
+    // Metodo per cambiare l'immagine successiva o precedente
+    nextImg(isNext) {
+      // Incrementa o decrementa il contatore in base al flag isNext
       isNext ? this.counter++ : this.counter--;
+
+      // Gestisci il loop delle immagini
       if (this.counter === this.images.length) {
-        this.counter = 0
-      } else if(this.counter < 0) {
+        this.counter = 0;
+      } else if (this.counter < 0) {
         this.counter = this.images.length - 1;
       }
     },
-    autoPlay(){
+    // Metodo per l'autoplay delle immagini
+    autoPlay() {
+      // Utilizza setInterval per chiamare nextImg ogni 3000 millisecondi (3 secondi)
       setInterval(() => {
-      if(this.autoPlayEnabled){
-         this.nextImg(true)
-          }
-        }, 3000);
-      }      
+        // Verifica se l'autoplay è abilitato prima di passare all'immagine successiva
+        if (this.autoPlayEnabled) {
+          this.nextImg(true);
+        }
+      }, 3000);
+    },
   },
-  mounted(){
-    this.autoPlay();
-  }
-}).mount('#app')
+  mounted() {
+    // Chiamato quando l'istanza Vue viene montata nel DOM
+    this.autoPlay(); // Avvia l'autoplay al momento del montaggio
+  },
+}).mount('#app'); // Monta l'applicazione Vue nell'elemento con ID 'app'
