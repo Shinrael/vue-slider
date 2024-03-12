526511945b27aae09ed9e34f87edeebcc3f66fac
Shinrael <mircoelena89@gmail.com>
2024-03-12 15:34:16 +0100

    [fix] name image

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ createApp({
   data(){
     return{
       images: [
-        '..\img\01.webp',
-        '..\img\02.webp',
-        '..\img\03.webp',
-        '..\img\04.webp',
-        '..\img\05.webp',
-
+        '..\img\spiderman.webp',
+        '..\img\stray.webp',
+        '..\img\ratchetclank.webp',
+        '..\img\fortnite.webp',
+        '..\img\avengers.webp',
       ]
     }
   },
